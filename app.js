e89ff453b05d949b1074abe22743d3aef32ebfa5
Ratul-nirvana <ratulbhattacharjee92@gmail.com>
2018-04-18 09:27:22 +0530

    Bug fixed

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-
 // Test runner script for the Database api code. 
 // Author : Ratul 
 
-
-var config = new config().getConfig();
-config.then(this.connectDatabase,
+var confModule = require('./Configuration.js');
+var config = new confModule().getConfig();
+config.then(connectDatabase,
 	function(err){
 		console.log(err);
  	});
 
 
-var connectDatabase=function(config){
+function connectDatabase(config){
 //console.log(config);
 let rdbmsModule = require('./RDBMS.js');
 let rdbms = new rdbmsModule(config.database);
-let table = {"name" : "tbl_user1"};
+console.log("Connected to RDBMS");
+/*let table = {"name" : "tbl_user1"};
 rdbms.dropTable(table);
 let table0 = {"name" : "tbl_user2"};
 rdbms.dropTable(table0);
 let table1 = {"name" : "tbl_user3"};
-rdbms.dropTable(table1);
+rdbms.dropTable(table1);*/
 }
